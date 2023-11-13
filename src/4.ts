e71c27b70c933455f5c84ecf3b803cbc779e3bd1
Oleksandrov1995 <olexandrov1995@gmail.com>
2023-11-13 12:43:26 +0100

    addHomework

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,52 @@
-const key = new Key();
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
 
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    } else {
+      Error;
+    }
+  }
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super();
+    this.key = key;
+  }
+  openDoor(enteredKey: Key): void {
+    if (enteredKey === this.key) {
+      this.door === true;
+    } else Error;
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
-
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
-
 export {};
